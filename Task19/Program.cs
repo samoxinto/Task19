ecaab660c206dc85644342b1c616c6adb7cea15c
Денис Степаненко <denis.stepanenko@hotmail.com>
2022-08-18 13:37:02 +0300

    Add project files.

---
+++ Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+
+            string number = Console.ReadLine();
+
+            string reversedStr = "";
+            for (int i = number.Length - 1; i >= 0; i--)
+                reversedStr += number[i];
+
+            if (number == reversedStr)
+                Console.WriteLine("Да");
+            else
+                Console.WriteLine("Нет");
+
+            Console.ReadLine();
+        }
+    }
+}
